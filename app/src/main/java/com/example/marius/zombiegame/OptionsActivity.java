8a7f1918d197772e9616418fc4ba1cc6c0e2eba1
GuieMigi <florinchiriac2004@yahoo.com>
2018-02-11 13:41:54 +0200

    Added OptionsActivity class and set the onClickListener to open the activity

---
+++ app/src/main/java/com/example/marius/zombiegame/OptionsActivity.java
@@ -0,0 +1,20 @@
+package com.example.marius.zombiegame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Florin on 11.02.2018.
+ */
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+
+
+}
